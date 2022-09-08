3e31a51ffed27b5610aceaa34b6e7dc2caec7340
Aneesh Sallaram <sallaram@unc.edu>
2022-09-08 13:27:35 -0400

    Lessons

---
+++ lessons/eightball.py
@@ -0,0 +1,12 @@
+from random import randint
+question : str = input("What is your yes/no question?")
+response: int = randint(0, 2)
+
+if response == 0:
+    print("Yes, def")
+elif response == 1:
+    print("Ask again later")
+elif response == 2:
+    print("Yes ofc")
+elif response == 3:
+    print("My sources say no")
---
+++ lessons/functions_def.py
@@ -0,0 +1,8 @@
+"""An example function definition."""
+
+from curses import ACS_DARROW
+
+
+print(my_max(1, 2))
+
+def my_max(a: int, b: int) 
